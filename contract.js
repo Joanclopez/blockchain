0fb8fd6e7818131d5f91ed40405bceac182b5b3a
eoinco <eoin.connolly@gmail.com>
2021-10-26 15:12:40 +0100

    instantiated web3

--- contract.js
+++ contract.js
@@ -1,11 +1,15 @@
 // this is a basic readonly contract interaction file
 const Web3 = require("web3")
 
-// https://ropsten.infura.io/v3/
 
 // today's lab
 
 // instantiate web3
+
+const rpcURL = "https://ropsten.infura.io/v3/";
+const web3 = new Web3(rpcURL);
+
+
 // get the ABI (interface) for our contract
 // connnect to our contract on ropsten
 // run some of the methods in our contract (using javascript)
