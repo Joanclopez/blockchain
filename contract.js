e494bd811fb71350bb1a2c3d2fe3ce70f397d4a7
eoinco <eoin.connolly@gmail.com>
2021-10-26 15:10:34 +0100

    setting up tasks for day

--- contract.js
+++ contract.js
@@ -1,6 +1,17 @@
 // this is a basic readonly contract interaction file
 const Web3 = require("web3")
 
+// https://ropsten.infura.io/v3/
+
+// today's lab
+
+// instantiate web3
+// get the ABI (interface) for our contract
+// connnect to our contract on ropsten
+// run some of the methods in our contract (using javascript)
+
+
+
 console.log("hello world?");
 
 
