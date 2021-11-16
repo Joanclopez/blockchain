aee34febef241f3dd916afc04e98728759042a74
eoinco <eoin.connolly@gmail.com>
2021-11-16 16:00:59 +0000

    added handlers

---
+++ handlers.js
@@ -0,0 +1,8 @@
+// create a web server
+
+// add a URL to the web server to do a transfer
+
+// let's use this web server to distribute our tokens
+
+// if there's time, let's add a docker container
+
