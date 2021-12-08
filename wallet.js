2a1de2ce68f1b6a7734007d93f1621943e0e73ef
eoinco <eoin.connolly@gmail.com>
2021-12-08 12:12:05 +0000

    hd wallet working

--- wallet.js
+++ wallet.js
@@ -42,6 +42,23 @@ const getHDWalletDetails = async() => {
     // converts the seed phrase into entropy
     let seed = await bip39.mnemonicToSeed(mnemonic);
 
+    let hdwallet = hdkey.fromMasterSeed(seed)
+
+    // takes in a 'path' to create a deterministic account
+    const path = "m/44'/60'/0'/0/0";
+    const wallet = hdwallet.derivePath(path).getWallet();
+    console.log(`wallet is ${wallet}`);
+
+    console.log("wallet public key: " + wallet.getPublicKeyString())
+
+    // hash the public key (keccak256)
+    const pubKey = wallet.getPublicKey();
+    const hashedPublicKey = keccak256(pubKey).toString('hex');
+    console.log(`hashed public key: 0x${hashedPublicKey}`);
+
+    // - drop the first 12 bytes
+    var ethAddress = hashedPublicKey.substring(24);
+    console.log(`eth address is 0x${ethAddress}`);
 }
 
 getHDWalletDetails()
