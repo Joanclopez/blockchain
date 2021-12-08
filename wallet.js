88c1e61fdea0021b93214401b18b27ccca54692b
eoinco <eoin.connolly@gmail.com>
2021-12-08 11:22:52 +0000

    generating keypair

--- wallet.js
+++ wallet.js
@@ -3,5 +3,15 @@
 // - hash the public key (keccak256)
 // - drop the first 12 bytes
 
-const Wallet = require("ethereujs-wallet").default;
+const Wallet = require("ethereumjs-wallet").default;
 const keccak256 = require("keccak256");
+
+
+const getWalletDetails = async() => {
+    console.log("getting details...");
+    const wallet = Wallet.generate();
+    console.log("generated wallet...");
+    console.log("wallet public key: " + wallet.getPublicKeyString())
+}
+
+getWalletDetails();
