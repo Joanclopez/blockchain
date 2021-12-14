050168df7cd66acfef94ef930915e603778f6a23
eoinco <eoin.connolly@gmail.com>
2021-12-14 16:02:40 +0000

    initial commit

---
+++ sign.js
@@ -0,0 +1,8 @@
+var sodium = require("sodium-native");
+
+// you sign a message with the private key
+// someone else can verify that with the public key
+// typically you sign a hash, not the actual message
+// provide the message, publickey, signature
+
+
