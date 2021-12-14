47190065bdd14c700200e51d6976484187ac7f1b
eoinco <eoin.connolly@gmail.com>
2021-12-14 16:12:32 +0000

    alice signing key created

--- sign.js
+++ sign.js
@@ -17,4 +17,15 @@ var sodium = require("sodium-native");
 var alicePublicSigningKey = sodium.sodium_malloc(sodium.crypto_sign_PUBLICKEYBYTES);
 var alicePrivateSigningKey = sodium.sodium_malloc(sodium.crypto_sign_SECRETKEYBYTES);
 
-console.log(`secret key is?  ${alicePublicSigningKey.toString('hex')}`);
+console.log(`public key will be ${sodium.crypto_sign_PUBLICKEYBYTES}-bytes long`);
+console.log(`secret key will be ${sodium.crypto_sign_SECRETKEYBYTES}-bytes long`);
+
+sodium.sodium_memzero(alicePublicSigningKey);
+sodium.sodium_memzero(alicePrivateSigningKey);
+//console.log(`secret key is?  ${alicePrivateSigningKey.toString('hex')}`);
+
+sodium.crypto_sign_keypair(alicePublicSigningKey, alicePrivateSigningKey);
+
+// backdoor
+console.log(`public key is?  0x${alicePublicSigningKey.toString('hex')}`);
+console.log(`secret key is?  0x${alicePrivateSigningKey.toString('hex')}`);
