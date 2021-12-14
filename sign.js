808787211549b83e38aaec1ad63ea2389254ed27
eoinco <eoin.connolly@gmail.com>
2021-12-14 16:25:08 +0000

    hashing message

--- sign.js
+++ sign.js
@@ -13,12 +13,12 @@ var sodium = require("sodium-native");
 // X25519 is used for ecdh
 // but.. you can convert C25519 to X25519
 
-// create Alice signing key
+// 1. create Alice signing key
 var alicePublicSigningKey = sodium.sodium_malloc(sodium.crypto_sign_PUBLICKEYBYTES);
 var alicePrivateSigningKey = sodium.sodium_malloc(sodium.crypto_sign_SECRETKEYBYTES);
 
-console.log(`public key will be ${sodium.crypto_sign_PUBLICKEYBYTES}-bytes long`);
-console.log(`secret key will be ${sodium.crypto_sign_SECRETKEYBYTES}-bytes long`);
+// console.log(`public key will be ${sodium.crypto_sign_PUBLICKEYBYTES}-bytes long`);
+// console.log(`secret key will be ${sodium.crypto_sign_SECRETKEYBYTES}-bytes long`);
 
 sodium.sodium_memzero(alicePublicSigningKey);
 sodium.sodium_memzero(alicePrivateSigningKey);
@@ -29,3 +29,24 @@ sodium.crypto_sign_keypair(alicePublicSigningKey, alicePrivateSigningKey);
 // backdoor
 console.log(`public key is?  0x${alicePublicSigningKey.toString('hex')}`);
 console.log(`secret key is?  0x${alicePrivateSigningKey.toString('hex')}`);
+
+// 2. sign something with the private signing key
+
+var message = "Alice is not Satoshi Nakamoto, for sure and for real";
+console.log(`message is ${message.length} characters long`);
+
+// hash the message
+var messageHash = sodium.sodium_malloc(sodium.crypto_hash_sha256_BYTES);
+console.log(`message hash will be ${sodium.crypto_hash_sha256_BYTES}-bytes long`);
+
+var messageBuffer = Buffer.from(message);
+console.log(`message buffer ${messageBuffer}`)
+
+sodium.crypto_hash_sha256(messageHash, messageBuffer)
+
+console.log(`message hash is: 0x${messageHash.toString('hex')}`)
+
+
+var aliceSignature = sodium.sodium_malloc(sodium.crypto_sign_BYTES);
+
+console.log(`alice's signature will be ${sodium.crypto_sign_BYTES}-bytes long`)
